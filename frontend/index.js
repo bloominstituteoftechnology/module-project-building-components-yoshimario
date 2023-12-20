6b96ca71bf41e21bea688ae409a6ac02f186489c
Sean Kipinä <paintballsean@gmail.com>
2023-12-20 16:07:43 +0200

    Added companyNameP and companyInforDiv elements to buildFooter.

--- frontend/index.js
+++ frontend/index.js
@@ -1,105 +1,165 @@
 function moduleProject3() {
-
   // 👉 TASK 1 - Write a `buildNav` component that returns a nav
 
   function buildNav(links) {
     //  ✨ do your magic here
-    const container = document.createElement('nav')
-    links.forEach(link => {
-      let a = document.createElement('a')
-      a.href = link.href
-      a.title = link.title
-      a.textContent = link.textContent
-      container.appendChild(a)
-    })
-    return container
+    const container = document.createElement("nav");
+    links.forEach((link) => {
+      let a = document.createElement("a");
+      a.href = link.href;
+      a.title = link.title;
+      a.textContent = link.textContent;
+      container.appendChild(a);
+    });
+    return container;
   }
 
   // ❗ DOM creation using your `buildNav` component (do not change):
-  document.querySelector('header').appendChild(buildNav([
-    { href: 'https://www.example.com', textContent: 'Home', title: 'Go to the home page' },
-    { href: 'https://www.example.com/about', textContent: 'About', title: 'Learn more about our company' },
-    { href: 'https://www.example.com/services', textContent: 'Services', title: 'View our available services' },
-    { href: 'https://www.example.com/blog', textContent: 'Blog', title: 'Read our latest blog posts' },
-    { href: 'https://www.example.com/contact', textContent: 'Contact', title: 'Get in touch with us' },
-  ]))
+  document.querySelector("header").appendChild(
+    buildNav([
+      {
+        href: "https://www.example.com",
+        textContent: "Home",
+        title: "Go to the home page",
+      },
+      {
+        href: "https://www.example.com/about",
+        textContent: "About",
+        title: "Learn more about our company",
+      },
+      {
+        href: "https://www.example.com/services",
+        textContent: "Services",
+        title: "View our available services",
+      },
+      {
+        href: "https://www.example.com/blog",
+        textContent: "Blog",
+        title: "Read our latest blog posts",
+      },
+      {
+        href: "https://www.example.com/contact",
+        textContent: "Contact",
+        title: "Get in touch with us",
+      },
+    ])
+  );
 
   // 👉 TASK 2A - Write a `buildLearnerCard` component that returns a card
 
   function buildLearnerCard(learner, languages) {
     //  ✨ do your magic here
-    const card = document.createElement('div');
-    card.classList.add('learner-card');
-   
-    const nameP = document.createElement('p');
-    nameP.textContent = learner.fullName
-    
-    const idElement = document.createElement('p');
-    idElement.textContent = `Learner ID: ${learner.id}`
-    
-    const dobP = document.createElement('p');
+    const card = document.createElement("div");
+    card.classList.add("learner-card");
+
+    const nameP = document.createElement("p");
+    nameP.textContent = learner.fullName;
+
+    const idElement = document.createElement("p");
+    idElement.textContent = `Learner ID: ${learner.id}`;
+
+    const dobP = document.createElement("p");
     dobP.textContent = `Date of Birth: ${learner.dateOfBirth}`;
-  
-    const favLangP = document.createElement('p');
-    const favLanguage = languages.find(lang => lang.id === learner.favLanguage)
+
+    const favLangP = document.createElement("p");
+    const favLanguage = languages.find(
+      (lang) => lang.id === learner.favLanguage
+    );
     favLangP.textContent = `Favorite Language: ${favLanguage.name}`;
 
-    [nameP, idElement, dobP, favLangP].forEach(p => {
+    [nameP, idElement, dobP, favLangP].forEach((p) => {
       card.appendChild(p);
     });
 
-    card.addEventListener('click', evt => {
-      document.querySelectorAll('.learner-card').forEach(card => {
-        card.classList.remove('active')
-      })
-      card.classList.add('active');
-    })
+    card.addEventListener("click", (evt) => {
+      document.querySelectorAll(".learner-card").forEach((card) => {
+        card.classList.remove("active");
+      });
+      card.classList.add("active");
+    });
 
-    return card
+    return card;
   }
 
   {
     // 👉 TASK 2B - Use the two variables below to make learner Cards, and put them in the DOM
 
     let languages = [
-      { id: 37, name: 'JavaScript', creator: 'Brendan Eich', year: 1995 },
-      { id: 82, name: 'Python', creator: 'Guido van Rossum', year: 1991 },
-      { id: 12, name: 'Java', creator: 'James Gosling', year: 1995 },
-      { id: 53, name: 'C#', creator: 'Microsoft Corporation', year: 2000 },
-      { id: 91, name: 'Ruby', creator: 'Yukihiro Matsumoto', year: 1995 }
-    ]
+      { id: 37, name: "JavaScript", creator: "Brendan Eich", year: 1995 },
+      { id: 82, name: "Python", creator: "Guido van Rossum", year: 1991 },
+      { id: 12, name: "Java", creator: "James Gosling", year: 1995 },
+      { id: 53, name: "C#", creator: "Microsoft Corporation", year: 2000 },
+      { id: 91, name: "Ruby", creator: "Yukihiro Matsumoto", year: 1995 },
+    ];
     let learners = [
-      { id: 24, fullName: 'Kenneth Fisher', dateOfBirth: '1990-01-01', favLanguage: 82 },
-      { id: 53, fullName: 'Jess Williams', dateOfBirth: '1985-05-10', favLanguage: 37 },
-      { id: 72, fullName: 'Brandon Nguyen', dateOfBirth: '1992-09-15', favLanguage: 53 },
-      { id: 41, fullName: 'Sabah Beydoun', dateOfBirth: '1988-03-25', favLanguage: 91 },
-      { id: 17, fullName: 'Daniel Castillo', dateOfBirth: '1995-11-05', favLanguage: 12 }
-    ]
+      {
+        id: 24,
+        fullName: "Kenneth Fisher",
+        dateOfBirth: "1990-01-01",
+        favLanguage: 82,
+      },
+      {
+        id: 53,
+        fullName: "Jess Williams",
+        dateOfBirth: "1985-05-10",
+        favLanguage: 37,
+      },
+      {
+        id: 72,
+        fullName: "Brandon Nguyen",
+        dateOfBirth: "1992-09-15",
+        favLanguage: 53,
+      },
+      {
+        id: 41,
+        fullName: "Sabah Beydoun",
+        dateOfBirth: "1988-03-25",
+        favLanguage: 91,
+      },
+      {
+        id: 17,
+        fullName: "Daniel Castillo",
+        dateOfBirth: "1995-11-05",
+        favLanguage: 12,
+      },
+    ];
     //  ✨ do your magic here
-    learners.forEach(learner => {
+    learners.forEach((learner) => {
       const learnerCard = buildLearnerCard(learner, languages);
-      document.querySelector('section').appendChild(learnerCard);
-    })
+      document.querySelector("section").appendChild(learnerCard);
+    });
   }
 
   // 👉 TASK 3 - Write a `buildFooter` component that returns a footer
 
   function buildFooter(footerData) {
     //  ✨ do your magic here
-    return document.createElement('footer')
+    const footer = document.createElement("footer");
+
+    let companyInfoDiv = document.createElement("div");
+    companyInfoDiv.classList.add("company-info");
+
+    let companyNameP = document.createElement("p");
+    companyNameP.classList.add("company-name");
+    companyNameP.textContent = footerData.companyName
+    
+    return footer
+
   }
 
   // ❗ DOM creation using your `buildFooter` component (do not change):
-  document.body.appendChild(buildFooter({
-    companyName: 'Bloom Institute of Technology',
-    address: '123 Main Street, City, Country',
-    contactEmail: 'info@example.com',
-    socialMedia: {
-      twitter: 'https://twitter.com/example',
-      facebook: 'https://www.facebook.com/example',
-      instagram: 'https://www.instagram.com/example',
-    },
-  }))
+  document.body.appendChild(
+    buildFooter({
+      companyName: "Bloom Institute of Technology",
+      address: "123 Main Street, City, Country",
+      contactEmail: "info@example.com",
+      socialMedia: {
+        twitter: "https://twitter.com/example",
+        facebook: "https://www.facebook.com/example",
+        instagram: "https://www.instagram.com/example",
+      },
+    })
+  );
 
   // 👉 TASK 4 - Clicking on the section should deactivate the active card
 
@@ -109,5 +169,6 @@ function moduleProject3() {
 // ❗ DO NOT CHANGE THIS CODE
 // ❗ DO NOT CHANGE THIS CODE
 // ❗ DO NOT CHANGE THIS CODE
-if (typeof module !== 'undefined' && module.exports) module.exports = { moduleProject3 }
-else moduleProject3()
+if (typeof module !== "undefined" && module.exports)
+  module.exports = { moduleProject3 };
+else moduleProject3();
